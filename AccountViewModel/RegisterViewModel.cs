89637849dedaffd9304eef2c0de1e438b700c4cc
Chris Connell <33101328+ChrisConnell1@users.noreply.github.com>
2018-03-26 12:00:16 -0700

    Add properties to Register viewmodel
    These will be set by the forms created in the Register view

---
+++ AccountViewModel/RegisterViewModel.cs
@@ -0,0 +1,7 @@
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
